ea9da98064723f3f6bf57cb92b418d6cf58693e5
superernie77 <superernie77@gmail.com>
2021-02-08 14:55:03 +0100

    Consumer Added

---
+++ consumer/src/main/java/com/test/consumers/MessageConsumer.java
@@ -0,0 +1,65 @@
+package com.test.consumers;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.oracle.tools.packager.Log;
+
+public class MessageConsumer {
+
+	private static final Logger logger = LoggerFactory.getLogger(MessageConsumer.class);
+
+	private KafkaConsumer<String, String> kafkaConsumer;
+
+	private String topicName = "test-topic";
+
+	public MessageConsumer(Map<String, Object> propsMap) {
+		kafkaConsumer = new KafkaConsumer<>(propsMap);
+	}
+
+	public static Map<String, Object> buildConsumerProperties() {
+		Map<String, Object> propsMap = new HashMap<String, Object>();
+
+		propsMap.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9093,localhost:9094");
+		propsMap.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		propsMap.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		propsMap.put(ConsumerConfig.GROUP_ID_CONFIG, "messageconsumer");
+
+		return propsMap;
+	}
+
+	public void pollKafka() {
+		kafkaConsumer.subscribe(List.of(topicName));
+		Duration timeOut = Duration.of(100, ChronoUnit.MILLIS);
+		try {
+			while (true) {
+				ConsumerRecords<String, String> records = kafkaConsumer.poll(timeOut);
+				records.forEach((record) -> {
+					logger.info("Consumer Record Key is {} and the value is {} and the partition is {}", record.key(),
+							record.value(), record.partition());
+				});
+			}
+		} catch (Exception ex) {
+			logger.error("exception during poll.");
+		} finally {
+			kafkaConsumer.close();
+		}
+
+	}
+
+	public static void main(String[] args) {
+		MessageConsumer messageConsumer = new MessageConsumer(buildConsumerProperties());
+		messageConsumer.pollKafka();
+	}
+
+}
